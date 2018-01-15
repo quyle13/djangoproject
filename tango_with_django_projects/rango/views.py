385910da32f00d0fb66bfd118d376c151b7f5a66
quyle13@yahoo.com <quyle13.@yahoo.com>
2018-01-15 20:38:27 +0000

    Chapter 11 Session and Cookie

--- tango_with_django_projects/rango/views.py
+++ tango_with_django_projects/rango/views.py
@@ -10,8 +10,10 @@ from django.contrib.auth import authenticate,login
 from django.http import HttpResponseRedirect,HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 def index(request):
+	#request.session.set_test_cookie()
 	#-likes: sort by descending order
 	#:5 slice and dice array
 	context_dict = {}
@@ -22,11 +24,37 @@ def index(request):
 	context_dict ['pages'] = top_viewed_page
 	#render function: take user input request
 	#template of file name
+
+	visits = request.session.get("visits")
+	if not visits:
+		visits = 1
+	reset_last_visit_time = False
 	
-	return render(request,'rango/index.html',context_dict)
+	last_visit = request.session.get("last_visit")
+	if last_visit:
+		last_visit_time = datetime.strptime(last_visit[:-7],"%Y-%m-%d %H:%M:%S")
+		if (datetime.now() - last_visit_time).seconds >5:
+			visits = visits+1
+			reset_last_visit_time = True
+	else:
+		reset_last_visit_time = True
+
+	if reset_last_visit_time:
+		request.session["last_visit"] = str(datetime.now())
+		request.session["visits"] = visits
+	
+	context_dict["visits"] = visits
+	response =  render(request,'rango/index.html',context_dict)
+	
+	
+	return response
 
 def about(request):
-	return render(request,'rango/about.html')
+	if request.session.get("visits"):
+		count = request.session.get("visits")
+	else:
+		count =0
+	return render(request,'rango/about.html',{"visits": count})
 
 
 def category(request,category_name_slug):
@@ -85,6 +113,10 @@ def add_page(request,category_name_slug):
 	return render(request,"rango/add_page.html",context_dict)
 
 def register(request):
+	#if request.session.test_cookie_worked():
+	#	print ">>>> TEST COOKIE WORKED"
+	#	request.session.delete_test_cookie()
+
 	registered = False
 	if request.method == "POST":
 		user_form = UserForm(data = request.POST)
